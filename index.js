ae48090656b95f21513c7f5b0b7ad9b84426dd58
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-04-24 00:43:30 -0700

    completed intial API setup

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const axios =require('axios')
+const axios = require('axios')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 exports.getGitHubContributionsHistory = async (username, year, startDate, EndDate) => {
-  const fetchedHTMLForUser = await axios.get(`https://github.com/users/${username}/contributions`)
-  console.log(fetchedHTMLForUser)
+  const fetchedURLForUser = await axios({
+    method: 'get',
+    url: `https://github.com/users/${username}/contributions`,
+    responseType: 'text',
+  })
+  const fetchedHTML = JSDOM.fragment(fetchedURLForUser.data).firstElementChild
+  let contrubutionsText = fetchedHTML.querySelector('h2').textContent.split(' ')
+  for (let i = 0; i < contrubutionsText.length; i++) {
+    contrubutionsText[i] = parseInt(contrubutionsText[i].replace(/,/g, ''))
+  }
+  const results = {
+    annualContributions: contrubutionsText.filter(Boolean).toString(),
+  }
+  return results
 }
